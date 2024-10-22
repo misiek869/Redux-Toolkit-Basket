a26e296b1a156e2b61c479078cf47f452fe1b99b
Michal Gawik <gawlik.zator@o2.pl>
2024-10-22 14:42:15 +0200

    config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{js,jsx,ts,tsx}', // Ścieżki do plików
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
