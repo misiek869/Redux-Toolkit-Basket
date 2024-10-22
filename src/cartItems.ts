fe7756dfd294f48eb2b8fe36a32478b9ebd11f97
Michal Gawik <gawlik.zator@o2.pl>
2024-10-22 13:48:21 +0200

    first commit

---
+++ src/cartItems.ts
@@ -0,0 +1,35 @@
+
+
+
+const cartItems = [
+	{
+		id: 'rec1JZlfCIBOPdcT2',
+		title: 'LEGO ICONS Corvette',
+		price: '99.99',
+		img: 'https://imgbox.com/RcioCPbw',
+		amount: 1,
+	},
+	{
+		id: 'recB6qcHPxb62YJ75',
+		title: 'LEGO Batmobile',
+		price: '139.99',
+		img: 'https://imgbox.com/wdU8PN2W',
+		amount: 1,
+	},
+	{
+		id: 'recdRxBsE14Rr2VuJ',
+		title: 'LEGO Ghostbusters Vehicle',
+		price: '79.99',
+		img: 'https://imgbox.com/7VznuoEl',
+		amount: 1,
+	},
+	{
+		id: 'recwTo160XST3PIoW',
+		title: 'LEGO ICONS Porsche 911',
+		price: '119.99 ',
+		img: 'https://imgbox.com/Llk71qLB',
+		amount: 1,
+	},
+]
+
+export default cartItems
