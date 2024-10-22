c7155b254b336975069240e0012c061039374bbf
Michal Gawik <gawlik.zator@o2.pl>
2024-10-22 14:34:36 +0200

    create

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,14 @@
+import { FaShoppingCart } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
+
 const Navbar = () => {
-	return <div></div>
+	return (
+		<nav>
+			<div>
+				<h3>Redux Toolkit Basket</h3>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
