a803051695d1bc3b1c0e3baed1b17aed5e27e9cc
Michal Gawik <gawlik.zator@o2.pl>
2024-10-23 05:16:51 +0200

    add roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		'./src/**/*.{js,jsx,ts,tsx}', // Ścieżki do plików
-	],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				roboto: ['Roboto', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
