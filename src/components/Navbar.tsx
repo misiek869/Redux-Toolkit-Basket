df9918feb8371bd4cde5921c9d6e6ffeefea8e95
Michal Gawik <gawlik.zator@o2.pl>
2024-10-23 05:14:30 +0200

    style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import { useSelector } from 'react-redux'
 
 const Navbar = () => {
 	return (
-		<nav className='bg-[var(--clr-primary)] py-5 px-8'>
+		<nav className='bg-neutral-100 py-5 px-8 border-b border-gray-500'>
 			<div className='w-full flex justify-center items-center mx-auto'>
-				<h3 className='mb-0 tracking-wider text-[var(--clr-red)]'>
+				<h3 className='mb-0 tracking-wider text-orange-600'>
 					Redux Toolkit Basket
 				</h3>
 			</div>
