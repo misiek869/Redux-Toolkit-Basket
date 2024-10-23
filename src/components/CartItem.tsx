d626ee315c37e40aefc8156e69009ee4d8ba4216
Michal Gawik <gawlik.zator@o2.pl>
2024-10-23 09:19:43 +0200

    change links

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-const CartItem = () => {
-	return <div>item</div>
+type CartItemProps = {
+	id: string
+	img: string
+	title: string
+	price: string
+	amount: number
+}
+
+const CartItem = ({ id, img, title, price, amount }: CartItemProps) => {
+	console.log(img)
+	return (
+		<article>
+			<img src={img} alt={title} />
+		</article>
+	)
 }
 
 export default CartItem
