99ec7907401c7bf788c06a67425e3c5e36851922
Michal Gawik <gawlik.zator@o2.pl>
2024-10-23 05:45:24 +0200

    change styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,13 +7,13 @@ const Navbar = () => {
 
 	return (
 		<nav className='bg-neutral-100 py-5 px-8 border-b border-gray-500 shadow-sm'>
-			<div className='w-full flex justify-center items-center mx-auto'>
+			<div className='w-full flex justify-between items-center mx-auto'>
 				<h3 className='mb-0 tracking-wider text-orange-600'>
 					Redux Toolkit Basket
 				</h3>
 				<div className='block relative'>
 					<CartIcon />
-					<div className='absolute rounded-full flex items-center justify-center bg-neutral-400 top-2 right-2 w-7 h-7'>
+					<div className='absolute rounded-full flex items-center justify-center bg-neutral-400 bottom-2 left-2 w-7 h-7'>
 						<p className='text-white mb-0 text-xl'>{amount}</p>
 					</div>
 				</div>
