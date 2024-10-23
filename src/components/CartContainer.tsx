8ed99140462a441bd1db218b181280a2a3a35914
Michal Gawik <gawlik.zator@o2.pl>
2024-10-23 06:18:20 +0200

    create body

--- src/components/CartContainer.tsx
+++ src/components/CartContainer.tsx
@@ -1,7 +1,23 @@
+import { useSelector } from 'react-redux'
 import CartItem from './CartItem'
 
 const CartContainer = () => {
-	const { cartI }
+	const { cartItems, amount, total } = useSelector(state => state.cart)
+
+	if (amount < 1) {
+		return (
+			<section className='min-h-[calc(100vh-120px)] w-[90vw] my-0 mx-auto mt-10 py-8 px-0 max-w-5xl'>
+				<header>
+					<h2 className='uppercase text-center mb-10 text-gray-800'>
+						your bag
+					</h2>
+					<h4 className='lowercase text-gray-800 mt-4 text-center'>
+						is currently empty
+					</h4>
+				</header>
+			</section>
+		)
+	}
 
 	return <div>container</div>
 }
