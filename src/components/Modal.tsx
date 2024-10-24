bbae5be49202a330153257e5e735f411d0e814b5
Michal Gawik <gawlik.zator@o2.pl>
2024-10-24 05:35:58 +0200

    create component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,15 @@
+const Modal = () => {
+	return (
+		<aside>
+			<div className=''>
+				<h3>Do you want to empty the cart?</h3>
+				<div className=''>
+					<button type='button'>yes</button>
+					<button type='button'>no</button>
+				</div>
+			</div>
+		</aside>
+	)
+}
+
+export default Modal
